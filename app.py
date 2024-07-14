2ca3ba69dc2f9a643cd93b6d3546f74c8e5928d4
Khang Nguyen <nguyendakcun@gmail.com>
2024-07-14 12:02:27 +0700

    merge with todolist

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask,render_template,request
 from markupsafe import escape
+import pymongo
+#===========================================================================================================
+connection_str="mongodb+srv://nguyendakacun:htUezXTkL0llRnwa@dangkhang.nbpxo57.mongodb.net/?retryWrites=true&w=majority&appName=DangKhang"
+try:
+    print("Connect done")
+    client = pymongo.MongoClient(connection_str)
+except Exception:
+    print("Error", Exception)
+db = client["mydatabase"]
+col = db['todolist']
+for x in col.find():
+    print(x)
+#===========================================================================================================
 app = Flask(__name__)
 
 '''dict chua mat khau va tai khoan nguoi dung'''
-users = {"Khang":"123456","username":"password2"}
+users = {"Khang":"123456","username":"password"}
 
 
 '''trang web dau tien'''
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("register.html")
 
 
 '''trang web dung de nhap mat khau va ten nguoi dung'''
@@ -20,7 +33,7 @@ def handle_post():
         password = request.form['password']
         print(username, password)
         if username in users and users[username] == password:
-            return '<h1>Welcome!!!</h1>'
+            return render_template("todolist.html")
         else:
             return '<h1>invalid credentials!</h1>'
     else:
