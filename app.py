cc3cc477d6c7848d9f0a9e1eb0527908a2eb8529
Khang Nguyen <nguyendakcun@gmail.com>
2024-07-20 11:02:25 +0700

    code css todolist and app.py

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect
 from markupsafe import escape
 import pymongo
+from werkzeug.security import check_password_hash,generate_password_hash
 #===========================================================================================================
 connection_str="mongodb+srv://nguyendakacun:htUezXTkL0llRnwa@dangkhang.nbpxo57.mongodb.net/?retryWrites=true&w=majority&appName=DangKhang"
+users_value={}
 try:
     print("Connect done")
     client = pymongo.MongoClient(connection_str)
 except Exception:
     print("Error", Exception)
 db = client["mydatabase"]
-col = db['todolist']
+col = db['user_data']
+
 for x in col.find():
+    user = x["user"]
+    password = x["password"]
     print(x)
+    users_value.update({user:password})
 #===========================================================================================================
 app = Flask(__name__)
 
 '''dict chua mat khau va tai khoan nguoi dung'''
-users = {"Khang":"123456","username":"password"}
+#users = {"Khang": generate_password_hash("123456"), "username": generate_password_hash("password")}
 
 
 '''trang web dau tien'''
@@ -28,15 +34,18 @@ def hello_world():
 '''trang web dung de nhap mat khau va ten nguoi dung'''
 @app.route("/handle_post",methods=['POST'])
 def handle_post():
+    error = None
     if request.method == "POST":
-        username = request.form['username']
-        password = request.form['password']
-        print(username, password)
-        if username in users and users[username] == password:
-            return render_template("todolist.html")
+        username = request.form["username"]
+        password = request.form["password"]
+        if username in users_value and users_value[user]==password:
+                # Successful login, redirect to todolist
+                return render_template("todolist.html")
         else:
-            return '<h1>invalid credentials!</h1>'
+            # Username not found
+            return render_template("login.html", error=error)  # Pass error message
     else:
-        return render_template('login html')
+        # Not a POST request (unexpected)
+        return render_template("login.html", error="Unexpected request method")                                            
 if __name__ == "__main__":
     app.run()
