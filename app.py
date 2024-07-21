54965e62ef5402ce9cfc0297381b7d591e135717
Khang Nguyen <nguyendakcun@gmail.com>
2024-07-21 10:48:12 +0700

    updated app.py,login2 change to login and change all color to red and code js for first page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect,url_for
 from markupsafe import escape
 import pymongo
 from werkzeug.security import check_password_hash,generate_password_hash
@@ -27,18 +27,29 @@ app = Flask(__name__)
 
 '''trang web dau tien'''
 @app.route("/")
-def hello_world():
+def first_page():
+    return render_template("first_page.html")
+@app.route('/register_rq')
+def register_rq():
+    return redirect(url_for('register'))
+@app.route("/register")
+def register():
     return render_template("register.html")
-
-
+@app.route('/login_rq')
+def login_rq():
+    return redirect(url_for('login'))
+@app.route("/login")
+def login():
+    return render_template("login.html")
 '''trang web dung de nhap mat khau va ten nguoi dung'''
-@app.route("/handle_post",methods=['POST'])
-def handle_post():
+@app.route("/handle_get",methods=['GET'])
+def handle_get():
     error = None
-    if request.method == "POST":
+    if request.method == "GET":
         username = request.form["username"]
         password = request.form["password"]
-        if username in users_value and users_value[user]==password:
+        print("username: "+ username+" password: "+ password)
+        if username in users_value and users_value[username]==password:
                 # Successful login, redirect to todolist
                 return render_template("todolist.html")
         else:
@@ -46,6 +57,20 @@ def handle_post():
             return render_template("login.html", error=error)  # Pass error message
     else:
         # Not a POST request (unexpected)
-        return render_template("login.html", error="Unexpected request method")                                            
+        return render_template("login.html", error="Unexpected request method")
+@app.route("/handle_post",methods=['GET','POST'])
+def handle_post():
+    error = None
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if username != "" and password !="":
+            add_user={"user": username,"password":password}
+            X = col.insert_one(add_user)
+            return render_template("todolist.html")
+        else:
+            # Username not found
+            return render_template("register.html")  # Pass error message      
+    return render_template("register.html")  # Pass error message      
 if __name__ == "__main__":
     app.run()
